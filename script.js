7c3d6eb60cab5caf4cb286491bd4450e6c2e3de2
prateek271 <prateekpatriotasme@gmail.com>
2022-05-30 04:26:13 +0530

    v2

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY>0);
 });
 
+var button = document.getElementsByClassName("button");
+var textBox = document.getElementsByClassName("email");
+textBox.addEventListener("keyup", function (event) {
 
+    // Checking if key pressed is ENTER or not
+    // if the key pressed is ENTER
+    // click listener on button is called
+    if (event.keyCode == 13) {
+        button.click();}
+    });
